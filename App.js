6ceb931a4337f339ddf05fecfffbe977943687e1
maegan <maegan@siena.edu>
2024-04-15 16:44:19 -0400

    end of last class

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
-import {
-  StatusBar,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-} from "react-native";
-import { RestaurantsScreen } from "./src/restaurants/screens/restaurants.screens";
+import { RestaurantsScreen } from "./src/restaurants/screens/restaurants.screen";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -21,19 +13,19 @@ export default function App() {
   let [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
-
   let [latoLoaded] = useLato({
     Lato_400Regular,
   });
 
-  if (!oswaldLoaded || !latoLoaded) {
-    return null;
-  }
+  if (!oswaldLoaded || !latoLoaded) return null;
 
   return (
-    <ThemeProvider theme={theme}>
-      <RestaurantsScreen />
+    <>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+        <ExpoStatusBar />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
